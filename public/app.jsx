1edead2b796efaaf02b03bacbcbd28feaea3ff5d
USER <kaemsuphakporn.k94@gmail.com>
2024-10-10 21:33:49 +0700

    Fetch Login/Register
    Fetch Login/Register

---
+++ public/app.jsx
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const postroutes = require('./routes/posts');
+const userRoute = require('./routes/user');
+
+cons
